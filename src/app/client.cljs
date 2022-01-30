5c151ed674f41588da5b9428ded73168c2892d8d
Chris Broome <chrisbroome7@gmail.com>
2022-01-29 20:53:30 -0500

    Tutorial 4: dom classes, component option map exploration

--- src/app/client.cljs
+++ src/app/client.cljs
@@ -2,7 +2,7 @@
   (:require
     [com.fulcrologic.fulcro.application :as app]
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
-    [com.fulcrologic.fulcro.dom :as dom]
+    [com.fulcrologic.fulcro.dom :as dom :refer [div h3 ul li label button]]
     [com.fulcrologic.fulcro.algorithms.merge :as merge]
     [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]))
 
@@ -15,7 +15,7 @@
    :ident         :car/id
    :initial-state {:car/id    :param/id
                    :car/model :param/model}}
-  (dom/div "Model: " model))
+  (div "Model: " model))
 
 (def ui-car (comp/factory Car {:keyfn :car/id}))
 
@@ -28,24 +28,38 @@
                    :person/cars [{:id 40 :model "Tacoma"}
                                  {:id 41 :model "Escort"}
                                  {:id 42 :model "F-150"}]}}
-  (dom/div
-    (dom/div "Name: " name)
-    (dom/div "Age: " age)
-    (dom/button {:onClick #(comp/transact! this [(make-older {:person/id id})])}
-                "Make Older")
-    (dom/h3 "Cars")
-    (dom/ul (map ui-car cars))))
+  (div :.ui.segment
+       (div :.ui.form
+            (div :.field
+                 (label "Name: ")
+                 name)
+            (div :.field
+                 (label "Age: ")
+                 age)
+            (h3 "Cars")
+            (ul (map ui-car cars)))))
 
 (def ui-person (comp/factory Person {:keyfn :person/id}))
 
+(defsc PersonList [this {:person-list/keys [people]}]
+  {:query [{:person-list/people (comp/get-query Person)}]
+   :ident (fn [_ _] [:component/id ::person-list])
+   :initial-state {:person-list/people [{:id 1 :name "Bob"}
+                                        {:id 2 :name "Sally"}]}}
+  (div
+    (h3 "People")
+    (map ui-person people)))
+
+(def ui-person-list (comp/factory PersonList))
+
 (defonce APP (app/fulcro-app))
 
-(defsc Sample [this {:root/keys [person]}]
-  {:query         [{:root/person (comp/get-query Person)}]
-   :initial-state {:root/person {:id 1 :name "Bob"}}}
-  (dom/div
-    (when person
-      (ui-person person))))
+(defsc Sample [this {:root/keys [people]}]
+  {:query         [{:root/people (comp/get-query PersonList)}]
+   :initial-state {:root/people {}}}
+  (div
+    (when people
+      (ui-person-list people))))
 
 (defn ^:export init []
   (app/mount! APP Sample "app"))
@@ -55,7 +69,7 @@
   (merge/merge-component! APP Person {:person/id  1
                                       :person/age 20})
   (comp/get-initial-state Sample)
-
+  (comp/component-options Person)
   (app/current-state APP)
   (comp/transact! APP [(make-older {:person/id 1})])
   )
