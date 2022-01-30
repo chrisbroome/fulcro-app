a2c3f1a9e2ade09e99be349e568b6a3723b7e4b8
Chris Broome <chrisbroome7@gmail.com>
2022-01-29 19:52:24 -0500

    Tutorial 3, part 2: Mutations

--- src/app/client.cljs
+++ src/app/client.cljs
@@ -3,7 +3,12 @@
     [com.fulcrologic.fulcro.application :as app]
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
     [com.fulcrologic.fulcro.dom :as dom]
-    [com.fulcrologic.fulcro.algorithms.merge :as merge]))
+    [com.fulcrologic.fulcro.algorithms.merge :as merge]
+    [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]))
+
+(defmutation make-older [{:person/keys [id]}]
+  (action [{:keys [state]}]
+          (swap! state update-in [:person/id id :person/age] inc)))
 
 (defsc Car [this {:car/keys [id model] :as props}]
   {:query         [:car/id :car/model]
@@ -26,6 +31,8 @@
   (dom/div
     (dom/div "Name: " name)
     (dom/div "Age: " age)
+    (dom/button {:onClick #(comp/transact! this [(make-older {:person/id id})])}
+                "Make Older")
     (dom/h3 "Cars")
     (dom/ul (map ui-car cars))))
 
@@ -45,6 +52,10 @@
 
 (comment
   (reset! (::app/state-atom APP) {})
-
+  (merge/merge-component! APP Person {:person/id  1
+                                      :person/age 20})
   (comp/get-initial-state Sample)
-)
+
+  (app/current-state APP)
+  (comp/transact! APP [(make-older {:person/id 1})])
+  )
